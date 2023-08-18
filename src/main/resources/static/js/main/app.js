7f561060b212eaf549b90fd14edd06495726bf15
stophyeon <jeongjihyeon401@gmail.com>
2023-08-18 16:04:27 +0900

    검색결과페이지

--- src/main/resources/static/js/main/app.js
+++ src/main/resources/static/js/main/app.js
@@ -1,66 +1,18 @@
-const findTitle = function (movies) {
-  
-  let search = document.getElementById("search-input").value.toLowerCase();
+const genreItems = document.querySelectorAll('.select-genre li');
 
-  if (search.length <= 0) {
-    alert("검색어를 입력해주세요.");
-  } else {
-    const filtermovie = movies.filter((movie) =>
-      movie.title.toLowerCase().includes(search)
-    );
+genreItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const isActive = item.classList.contains('active');
 
-    if (filtermovie.length === 0) {
-      alert("검색어에 해당하는 영화가 없습니다.");
-    } else {
-      displaymovies(filtermovie);
-    }
-  }
-};
+    genreItems.forEach(otherItem => {
+      otherItem.classList.remove('active');
+    });
 
-function setEventListeners(movies) {
-  const form = document.querySelector(".search");
-  // 검색창에 입력 수행 시
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    findTitle(movies);
-  });
-}
-
-window.displayMovies = displaymovies;
-window.movieData = movieData;
-
-// main 데이터 갖고오면 여기에 담아지게? ★
-loadmovies().then((movies) => {
-  movieData(movies);
-  displaymovies(movies);
-  setEventListeners(movies);
-  orderBygenre(movies);
-});
-
-// 장르에 따른 영화 분류 함수 -> ★에서 가져오는식
-function orderBygenre(movies) {
-
-const dropdown = document.querySelector(".select-genre");
-
-// 장르 선택에 따라 영화 필터링 함수
-const filterAndDisplayMovies = (selectedOption, movies) => {
-  const filterMovies = movies.filter((movie) => {
-    const genres = movie.genres; // 영화 장르 가져오기
-
-    if (selectedOption === "all") {
-      return true; 
-    } else if (genres.includes(selectedOption)) {
-      return true; 
+    if (!isActive) {
+      item.classList.add('active');
     }
 
-    return false; 
+    const selectedGenre = item.getAttribute('data-value');
+    console.log('선택한 장르:', selectedGenre);
   });
-
-  displaymovies(filterMovies);
-};
-
-dropdown.addEventListener("change", function () {
-  const selectedOption = dropdown.value;
-  filterAndDisplayMovies(selectedOption, movies);
 });
-};
---
+++ src/main/resources/static/js/searchpage/searchpg.js
@@ -0,0 +1,17 @@
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const searchInput = document.getElementById('search-input');
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+
+  const searchMessage = document.getElementById('search-message');
+  if (searchTerm) {
+    searchMessage.textContent = `"${searchTerm}" 작품을 찾으시나요?`;
+    searchMessage.classList.remove('hidden');
+  } else {
+    searchMessage.classList.add('hidden');
+  }
+
+});
